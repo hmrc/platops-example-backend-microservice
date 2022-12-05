73638109f620ab3b1add6cfa22ecda854a08ca6d
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2022-12-05 17:56:23 +0000

    testing bobby

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,11 +10,13 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % hmrcMongoVersion,
+    "com.typesafe.akka" %% "akka-actor"                % "2.7.0",
     PlayImport.ws
   )
 
   val test = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-28"    % bootstrapPlayVersion % Test,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28"   % hmrcMongoVersion     % Test
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28"   % hmrcMongoVersion     % Test,
+    "uk.gov.hmrc"       %% "webdriver-factory"         % "0.37.0"             % Test
   )
 }
